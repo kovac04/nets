01fe0a85f7816562bb7e9771925d279df70f7bdb
Uros Kovacevic <kovac@Uros2.local>
2022-09-10 12:27:18 -0700

    bigram

--- makemore.py
+++ makemore.py
@@ -1,17 +1,17 @@
 from unicodedata import name
 
-
 names = open("names.txt" , "r").read().splitlines()
 
 print(names[:10])
 
 
-#Goes through the bi gram of all chars in names, including <S>(start)
-#and <E>(end) chars
+#Goes through the bigram of all chars in names, including <S>(start)
+#and <E>(end) chars and every time they occur, it counts
 b = {}
-for w in names[:1]:
+for w in names:
     chs = ['<S>'] + list(w) + ['<E>']
     for ch1, ch2 in zip(chs, chs[1:]):
         bigram = (ch1, ch2)
         b[bigram] = b.get(bigram, 0) + 1
-        print(ch1, ch2)
+
+print(sorted(b.items(),key = lambda kv: -kv[1]))
