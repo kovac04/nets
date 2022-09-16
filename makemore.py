616f46935643eafd441ce9b04bb4b60e72a48880
Uros Kovacevic <kovac@d142-058-081-020.wireless.sfu.ca>
2022-09-16 09:45:08 -0700

    notebook start

--- makemore.py
+++ makemore.py
@@ -1,17 +1,25 @@
 from unicodedata import name
-
+import torch
+import matplotlib
 names = open("names.txt" , "r").read().splitlines()
-
-print(names[:10])
 
 
 #Goes through the bigram of all chars in names, including <S>(start)
 #and <E>(end) chars and every time they occur, it counts
 b = {}
+
 for w in names:
-    chs = ['<S>'] + list(w) + ['<E>']
+    chs = ['.'] + list(w) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
+
+
         bigram = (ch1, ch2)
         b[bigram] = b.get(bigram, 0) + 1
 
-print(sorted(b.items(),key = lambda kv: -kv[1]))
+sorted(b.items(),key = lambda kv: -kv[1])
+
+chars = sorted(set((list(''.join(names)))))
+
+stoi = {s:i for i,s in enumerate(chars)}
+stoi['.'] = 26
+print(stoi)
